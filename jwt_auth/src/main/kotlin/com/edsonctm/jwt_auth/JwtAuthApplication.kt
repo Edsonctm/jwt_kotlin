c773531220b4b30ea07354d0ab95204103a76593
Edsonctm <Edson_ctm@hotmail.com>
2023-01-27 20:56:14 -0300

    first commit

---
+++ jwt_auth/src/main/kotlin/com/edsonctm/jwt_auth/JwtAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.edsonctm.jwt_auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtAuthApplication>(*args)
+}
